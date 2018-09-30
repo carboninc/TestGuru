b58d4bcfa2b574448cf061b3ac6e5fe12b77e23c
Alastar Corr <house.labs@gmail.com>
2018-09-30 19:03:11 +0600

    Add Default Values For Tests And Answers

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_30_125337) do
+ActiveRecord::Schema.define(version: 2018_09_30_130102) do
 
   create_table "answers", force: :cascade do |t|
     t.string "title", null: false
-    t.boolean "correct", null: false
+    t.boolean "correct", default: false, null: false
     t.integer "question_id_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -37,7 +37,7 @@ ActiveRecord::Schema.define(version: 2018_09_30_125337) do
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
-    t.integer "level", null: false
+    t.integer "level", default: 1, null: false
     t.integer "category_id_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
