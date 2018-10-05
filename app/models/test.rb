5c52ac39ffafa192bb8a4406053f04cb991285c8
Alastar Corr <house.labs@gmail.com>
2018-10-05 20:16:49 +0600

    Scope Fot Tests Level

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,6 +5,10 @@ class Test < ApplicationRecord
   has_many :users, through: :tests_users
   has_many :questions
 
+  scope :easy, -> { where(level: 0..1) }
+  scope :medium, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+
   def self.by_category(category)
     joins(:category).where(categories: { title: category }).order('title DESC')
   end
