9eb62e36504ce556e2f511d8ec0576fbee1c0fc6
Alastar Corr <house.labs@gmail.com>
2018-10-05 23:49:56 +0600

    Fix

--- app/models/test.rb
+++ app/models/test.rb
@@ -11,13 +11,10 @@ class Test < ApplicationRecord
   }
   validates :level, numericality: { only_integer: true, greater_than: 0 }
 
-  scope :easy, -> { where(level: 0..1) }
-  scope :medium, -> { where(level: 2..4) }
-  scope :hard, -> { where(level: 5..Float::INFINITY) }
-
   scope :by_levels, ->(*levels) { where(level: levels) }
+  scope :easy, -> { by_levels(0..1) }
+  scope :medium, -> { by_levels(2..4) }
+  scope :hard, -> { by_levels(5..Float::INFINITY) }
 
-  scope :by_category, lambda { |category|
-    joins(:category).where(categories: { title: category }).order('title DESC')
-  }
+  scope :by_category, ->(category) { joins(:category).where(categories: { title: category }).order(:title) }
 end
