8162b4dac3dfc5e0b902e93048233fcc110a981c
Alastar Corr <house.labs@gmail.com>
2018-10-05 22:28:56 +0600

    Add Validation Answers Count

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,6 +2,13 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   validates :title, presence: true
+  validate :answers_count, on: :create
 
   scope :correct_answer, -> { where(correct: true) }
+
+  private
+
+  def answers_count
+    errors.add(:answers, 'У вопроса не может быть более 4 ответов') if question.answers.length.to_i < 5
+  end
 end
