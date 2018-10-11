5b07647d7a3d2dc39286233d0c5ca377ef38b735
Alastar Corr <house.labs@gmail.com>
2018-10-11 14:02:52 +0600

    Add Question Not Found Exception

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,8 @@ class QuestionsController < ApplicationController
   before_action :find_test, only: %i[index create]
   before_action :find_question, only: %i[show destroy]
 
+  rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
+
   def index
     questions = @test.questions.pluck(:body)
     render plain: questions.join("\n")
@@ -31,5 +33,9 @@ class QuestionsController < ApplicationController
 
   def find_question
     @question = Question.find(params[:id])
+  end
+
+  def rescue_with_question_not_found
+    render plain: 'Такой вопрос не найден.'
   end
 end
