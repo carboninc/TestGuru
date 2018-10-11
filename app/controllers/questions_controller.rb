0d5e2db65fc2f9775fb2d5b3e37d39b71b243bba
Alastar Corr <house.labs@gmail.com>
2018-10-11 13:57:47 +0600

    Question Controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,35 @@
 class QuestionsController < ApplicationController
+  before_action :find_test, only: %i[index create]
+  before_action :find_question, only: %i[show destroy]
+
   def index
-    render json: { questions: Question.all }
+    questions = @test.questions.pluck(:body)
+    render plain: questions.join("\n")
+  end
+
+  def show
+    render plain: @question.body
+  end
+
+  def create
+    @test.questions.create(question_params)
+  end
+
+  def destroy
+    @question.destroy
+  end
+
+  private
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
   end
 end
