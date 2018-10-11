f77a3a2b57a038c2dc52a01dfccce694437aa838
Alastar Corr <house.labs@gmail.com>
2018-10-11 21:12:19 +0600

    Fix

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -14,8 +14,8 @@ class QuestionsController < ApplicationController
   end
 
   def create
-    @question = @test.questions.create(question_params)
-    if @question.save
+    question = @test.questions.new(question_params)
+    if question.save
       render plain: 'Вопрос создан'
     else
       render plain: 'Ошибка, не заполнены данные'
