743bb6544057d586304413d3f684b82d8d0789c2
Alastar Corr <house.labs@gmail.com>
2018-10-15 21:53:15 +0600

    Add ActiveRecord Locale | Create Questions Pages | Navigation

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,30 +1,50 @@
 class QuestionsController < ApplicationController
-  before_action :find_test, only: %i[index create]
+  before_action :find_test, only: %i[index new create]
   before_action :find_question, only: %i[show destroy]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
 
   def index
-    questions = @test.questions.pluck(:body)
-    render plain: questions.join("\n")
+    @questions = @test.questions
   end
 
   def show
-    render plain: @question.body
+    @question = Question.find(params[:id])
+  end
+
+  def new
+    @question = @test.questions.new
+  end
+
+  def edit
+    @question = Question.find(params[:id])
   end
 
   def create
-    question = @test.questions.new(question_params)
-    if question.save
-      render plain: 'Вопрос создан'
+    @question = @test.questions.new(question_params)
+
+    if @question.save
+      redirect_to test_questions_path(@test)
     else
-      render plain: 'Ошибка, не заполнены данные'
+      render :new
+    end
+  end
+
+  def update
+    @question = Question.find(params[:id])
+
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render :edit
     end
   end
 
   def destroy
+    @question = Question.find(params[:id])
+
     @question.destroy
-    render plain: 'Вопрос удален'
+    redirect_to test_questions_path(@question.test_id)
   end
 
   private
