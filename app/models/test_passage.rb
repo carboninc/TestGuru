9299cb8f777b24a1bbbefaf134daba0878bc8a22
Alastar Corr <house.labs@gmail.com>
2018-10-20 18:03:02 +0600

    Implement The Next Question Via Callback

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,6 +4,7 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_update :before_update_next_question, on: :update
 
   def completed?
     current_question.nil?
@@ -11,8 +12,6 @@ class TestPassage < ApplicationRecord
 
   def accept!(answer_ids)
     self.correct_questions += 1 if correct_answer?(answer_ids)
-
-    self.current_question = next_question
     save!
   end
 
@@ -22,6 +21,10 @@ class TestPassage < ApplicationRecord
     self.current_question = test.questions.first if test.present?
   end
 
+  def before_update_next_question
+    self.current_question = test.questions.order(:id).where('id > ?', current_question.id).first
+  end
+
   def correct_answer?(answer_ids)
     (correct_answers.ids.sort == Array(answer_ids).map(&:to_i).sort) &&
       correct_answers.count == answer_ids.count
@@ -29,9 +32,5 @@ class TestPassage < ApplicationRecord
 
   def correct_answers
     current_question.answers.correct
-  end
-
-  def next_question
-    test.questions.order(:id).where('id > ?', current_question.id).first
   end
 end
