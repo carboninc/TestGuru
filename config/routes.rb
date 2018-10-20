ca1ed8ffb7e04fe6c125a3475ba9b7130bc9d5f1
Alastar Corr <house.labs@gmail.com>
2018-10-20 13:01:38 +0600

    Add Answer Logic | Delete Index In Question Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  root 'tests#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :tests do
-    resources :questions, shallow: true
+    resources :questions, shallow: true, except: :index do
+      resources :answers, shallow: true
+    end
   end
 end
