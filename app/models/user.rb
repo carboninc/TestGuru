9b320a7716d009c5c10f705607013575f62d0258
Alastar Corr <house.labs@gmail.com>
2018-10-24 13:57:37 +0600

    Add Authenticate Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :test_passages
   has_many :tests, through: :test_passages
 
-  validates :email, presence: true
+  has_secure_password
 
   def test_list(*levels)
     tests.by_levels(levels)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_20_074132) do
+ActiveRecord::Schema.define(version: 2018_10_24_050239) do
 
   create_table "answers", force: :cascade do |t|
     t.string "title", null: false
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2018_10_20_074132) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "email"
+    t.string "password_digest"
   end
 
 end
