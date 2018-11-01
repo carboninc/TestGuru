b0d75f7f16015f7effb3433bb3aa5abc2cbf23ba
Alastar Corr <house.labs@gmail.com>
2018-11-01 12:19:38 +0600

    Add Devise Helper For Redirect Admin Users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(Admin)
+      admin_tests_path
+    else
+      root_path
+    end
+  end
 end
