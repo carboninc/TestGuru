27c411d0172ca890362c725c39cb2b55c7c03199
Alastar Corr <house.labs@gmail.com>
2018-11-01 12:47:09 +0600

    Add First And Last Name During Registration | Flash Notice After Sign In

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -3,10 +3,18 @@ class Admin::BaseController < ApplicationController
 
   before_action :authenticate_user!
   before_action :admin_required!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password) }
+  end
 
   private
 
   def admin_required!
-    redirect_to root_path, alert: 'Вы не авторизованы для просмотра этой страницы' unless current_user.is_a?(Admin)
+    redirect_to root_path, alert: 'У вас нет прав для просмотра этой страницы' unless current_user.is_a?(Admin)
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
+    flash[:notice] = "Привет, #{resource.first_name} #{resource.last_name}"
     if resource.is_a?(Admin)
       admin_tests_path
     else
