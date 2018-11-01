1962438e1ce165a49459bc2677d60ea9472f8e86
Alastar Corr <house.labs@gmail.com>
2018-11-01 19:49:28 +0600

    Config Locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_locale
+
+  def default_url_options
+    { lang: I18n.locale }
+  end
 
   def after_sign_in_path_for(resource)
     flash[:notice] = "Привет, #{resource.first_name} #{resource.last_name}"
@@ -15,5 +20,11 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password) }
     devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password) }
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
