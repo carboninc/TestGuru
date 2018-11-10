882f97a2a7412073b8f14907325e77dc6a50c7cc
Alastar Corr <house.labs@gmail.com>
2018-11-10 17:27:28 +0600

    JS Password Validation

---
+++ app/assets/javascripts/utilities/password_validation.js
@@ -0,0 +1,9 @@
+var checkPassword = function() {
+  if (document.getElementById('user_password').value == document.getElementById('user_password_confirmation').value) {
+    document.getElementById('message').style.color = 'green';
+    document.getElementById('message').innerHTML = 'Пароли совпадают!';
+  } else {
+    document.getElementById('message').style.color = 'red';
+    document.getElementById('message').innerHTML = 'Пароли не совпадают';
+  }
+}
