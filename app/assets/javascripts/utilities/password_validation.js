a572b61ed8259625b20dc0196274ed1209347dc6
Alastar Corr <house.labs@gmail.com>
2018-11-15 15:12:23 +0600

    Fix: Password Validation JS | Version: 0.14.1

--- app/assets/javascripts/utilities/password_validation.js
+++ app/assets/javascripts/utilities/password_validation.js
@@ -1,5 +1,20 @@
-var checkPassword = function() {
-  if (document.getElementById('user_password').value == document.getElementById('user_password_confirmation').value) {
+document.addEventListener('turbolinks:load', function() {
+  var password = document.querySelector('#user_password');
+  var passwordConfirmation = document.querySelector('#user_password_confirmation');
+
+  if (password && passwordConfirmation) {
+    password.addEventListener('keyup', checkPassword);
+    passwordConfirmation.addEventListener('keyup', checkPassword);
+  }
+});
+
+function checkPassword() {
+  var password = document.getElementById('user_password').value;
+  var passwordConfirmation = document.getElementById('user_password_confirmation').value;
+
+  if (password == '' || passwordConfirmation == '') {
+    return '';
+  } else if (password == passwordConfirmation) {
     document.getElementById('message').style.color = 'green';
     document.getElementById('message').innerHTML = 'Пароли совпадают!';
   } else {
