1da7cd5925b73d4469b2c693d3a8c0b112a06f87
Alastar Corr <house.labs@gmail.com>
2018-11-16 17:03:38 +0600

    Add Mail Production Config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,18 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "TestGuru_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'morning-garden-76174.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.yandex.ru',
+    port: 465,
+    domain: 'yandex.ru',
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: Rails.application.credentials.yandex_mail[:login],
+    password: Rails.application.credentials.yandex_mail[:password],
+    ssl: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
