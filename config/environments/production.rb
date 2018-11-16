5001cbf5317af6f5071a872758d3f60f97c78ea8
Alastar Corr <house.labs@gmail.com>
2018-11-16 17:27:43 +0600

    Change Action Mailer Default Url Options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "TestGuru_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'morning-garden-76174.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'testguru777.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.yandex.ru',
