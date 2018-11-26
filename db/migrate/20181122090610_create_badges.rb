5e97fdba9be525b6e66b3594d88c4067822215ea
Alastar Corr <house.labs@gmail.com>
2018-11-26 14:01:23 +0600

    Fix: Badges Migration

--- db/migrate/20181122090610_create_badges.rb
+++ db/migrate/20181122090610_create_badges.rb
@@ -1,6 +1,6 @@
 class CreateBadges < ActiveRecord::Migration[5.2]
   def change
-    create_table :user_badges do |t|
+    create_table :badges do |t|
       t.string :name, null: false
       t.string :image, null: false
       t.integer :rule, null: false, default: 0
